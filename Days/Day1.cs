6dddb7b44b8e32a9e6bbf68008caf9dce7b48aea
Mateusz Bąkała <maba@netcompany.com>
2022-12-01 06:14:44 +0100

    Day 1

--- Days/Day1.cs
+++ Days/Day1.cs
@@ -1,21 +1,22 @@
 ﻿using AdventOfCode2022.Input;
+using AdventOfCode2022.Models;
 
 namespace AdventOfCode2022.Days
 {
-    public class Day1 : AocDay<string>
+    public class Day1 : AocDay<ElvesCalories[]>
     {
-        public Day1(IInputParser<string> inputParser) : base(inputParser)
+        public Day1(IInputParser<ElvesCalories[]> inputParser) : base(inputParser)
         {
         }
 
-        protected override void Part1(string input)
+        protected override void Part1(ElvesCalories[] input)
         {
-            Console.WriteLine(input);
+            Console.WriteLine(input.Max(x => x.Calories.Sum()));
         }
 
-        protected override void Part2(string input)
+        protected override void Part2(ElvesCalories[] input)
         {
-            Console.WriteLine(input);
+            Console.WriteLine(input.OrderByDescending(x => x.Calories.Sum()).Take(3).Sum(x => x.Calories.Sum()));
         }
     }
 }
---
+++ Input/ElvesCaloriesParser.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode2022.Models;
+
+namespace AdventOfCode2022.Input
+{
+    public class ElvesCaloriesParser : IInputParser<ElvesCalories[]>
+    {
+        public ElvesCalories[] ParseInput(string input)
+        {
+            return input.Split($"{Environment.NewLine}{Environment.NewLine}")
+                .Select(i => i.Split(Environment.NewLine).Select(int.Parse).ToArray())
+                .Select(s => new ElvesCalories(s)).ToArray();
+        }
+    }
+}
