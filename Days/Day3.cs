9901fb3ee52a5a3337bf5bb473fe2247d8a2a478
Mateusz Bąkała <maba@netcompany.com>
2022-12-03 09:27:03 +0100

    Day 3

--- Days/Day3.cs
+++ Days/Day3.cs
@@ -2,20 +2,43 @@
 
 namespace AdventOfCode2022.Days
 {
-    public class Day3 : AocDay<string>
+    public class Day3 : AocDay<string[]>
     {
-        public Day3(IInputParser<string> inputParser) : base(inputParser)
+        public Day3(IInputParser<string[]> inputParser) : base(inputParser)
         {
         }
 
-        protected override void Part1(string input)
+        protected override void Part1(string[] input)
         {
-            Console.WriteLine(input);
+            int priorities = 0;
+            foreach (var line in input)
+            {
+                bool toBreak = false;
+                for (int i = 0; i <= line.Length / 2 && !toBreak; i++) 
+                    for (int j = line.Length - 1; j >= line.Length / 2; j--)
+                        if (line[i] == line[j])
+                        {
+                            priorities += (char.IsUpper(line[i]) ? 27 + line[i] - 'A' : line[i] - 'a' + 1);
+                            toBreak = true;
+                            break;
+                        }
+            }
+            Console.WriteLine(priorities);
         }
 
-        protected override void Part2(string input)
+        protected override void Part2(string[] input)
         {
-            Console.WriteLine(input);
+            int priorities = 0;
+            foreach (var lines in input.Chunk(3))
+            {
+                foreach (var c in lines[0])
+                    if (lines[1].Contains(c) && lines[2].Contains(c))
+                    {
+                        priorities += (char.IsUpper(c) ? 27 + c - 'A' : c - 'a' + 1);
+                        break;
+                    }
+            }
+            Console.WriteLine(priorities);
         }
     }
 }
