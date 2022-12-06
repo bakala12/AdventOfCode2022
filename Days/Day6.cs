bf221e04e905a0b2e4ab32face71029336b1afa6
Mateusz Bąkała <maba@netcompany.com>
2022-12-06 08:37:54 +0100

    Day 6

--- Days/Day6.cs
+++ Days/Day6.cs
@@ -10,12 +10,35 @@ namespace AdventOfCode2022.Days
 
         protected override void Part1(string input)
         {
-            Console.WriteLine(input);
+            for(int i = 3; i < input.Length; i++)
+            {
+                if(AreDifferent(input, i, 4))
+                {
+                    Console.WriteLine(i+1);
+                    break;
+                }
+            }
         }
 
         protected override void Part2(string input)
         {
-            Console.WriteLine(input);
+            for (int i = 13; i < input.Length; i++)
+            {
+                if (AreDifferent(input, i, 14))
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+            }
+        }
+
+        private static bool AreDifferent(string input, int position, int count)
+        {
+            for(int i = position - count + 1; i < position; i++)
+                for(int j = position; j > i; j--)
+                    if (input[i] == input[j])
+                        return false;
+            return true;
         }
     }
 }
