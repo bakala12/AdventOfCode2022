329ae94823aef46f896d0f18d66a0621ef7ec898
Mateusz Bąkała <maba@netcompany.com>
2022-12-08 06:26:54 +0100

    Day 8

--- Days/Day8.cs
+++ Days/Day8.cs
@@ -2,20 +2,102 @@
 
 namespace AdventOfCode2022.Days
 {
-    public class Day8 : AocDay<string>
+    public class Day8 : AocDay<int[,]>
     {
-        public Day8(IInputParser<string> inputParser) : base(inputParser)
+        public Day8(IInputParser<int[,]> inputParser) : base(inputParser)
         {
         }
 
-        protected override void Part1(string input)
+        protected override void Part1(int[,] input)
         {
-            Console.WriteLine(input);
+            bool[,] visible = new bool[input.GetLength(0), input.GetLength(1)];
+            for(int i = 0; i < input.GetLength(0); i++)
+            {
+                int h = -1;
+                for(int j = 0; j < input.GetLength(1); j++)
+                {
+                    if (input[i,j] > h)
+                    {
+                        visible[i, j] = true;
+                        h = input[i,j];
+                    }
+                }
+                h = -1;
+                for(int j = input.GetLength(1)-1; j >= 0; j--)
+                {
+                    if (input[i, j] > h)
+                    {
+                        visible[i, j] = true;
+                        h = input[i, j];
+                    }
+                }
+            }
+            for (int j = 0; j < input.GetLength(1); j++)
+            {
+                int h = -1;
+                for(int i = 0; i < input.GetLength(0); i++)
+                {
+                    if (input[i, j] > h)
+                    {
+                        visible[i, j] = true;
+                        h = input[i, j];
+                    }
+                }
+                h = -1;
+                for(int i = input.GetLength(0) - 1; i>=0; i--)
+                {
+                    if (input[i, j] > h)
+                    {
+                        visible[i, j] = true;
+                        h = input[i, j];
+                    }
+                }
+            }
+            int c = 0;
+            for (int i = 0; i < input.GetLength(0); i++)
+                for (int j = 0; j < input.GetLength(1); j++)
+                    if (visible[i, j])
+                        c++;
+            Console.WriteLine(c);
         }
 
-        protected override void Part2(string input)
+        protected override void Part2(int[,] input)
         {
-            Console.WriteLine(input);
+            int c = -1;
+            for (int i = 0; i < input.GetLength(0); i++)
+                for (int j = 0; j < input.GetLength(1); j++)
+                    c = Math.Max(c, CountTrees(input, i, j));
+            Console.WriteLine(c);
+        }
+
+        private static int CountTrees(int[,] input, int i, int j)
+        {
+            int a = 0, b = 0, c = 0, d = 0;
+            for (int ii = i-1; ii >= 0; ii--)
+            {
+                a++;
+                if (input[ii, j] >= input[i,j])
+                    break;
+            }
+            for (int ii = i + 1; ii < input.GetLength(0); ii++)
+            {
+                b++;
+                if (input[ii, j] >= input[i, j])
+                    break;
+            }
+            for(int jj = j - 1; jj >= 0; jj--)
+            {
+                c++;
+                if (input[i, jj] >= input[i, j])
+                    break;
+            }
+            for (int jj = j + 1; jj < input.GetLength(1); jj++)
+            {
+                d++;
+                if (input[i, jj] >= input[i, j])
+                    break;
+            }
+            return a * b * c * d;
         }
     }
 }
---
+++ Input/TreeParser.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode2022.Input
+{
+    public class TreeParser : IInputParser<int[,]>
+    {
+        public int[,] ParseInput(string input)
+        {
+            var s = input.Split(Environment.NewLine);
+            var a = new int[s.Length, s[0].Length];
+            for (int i = 0; i < s.Length; i++)
+                for (int j = 0; j < s[0].Length; j++)
+                    a[i, j] = s[i][j] - '0';
+            return a;
+        }
+    }
+}
