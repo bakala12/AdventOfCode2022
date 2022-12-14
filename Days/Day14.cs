d1769b591a79f2d34c9328cc876558e3495e5289
Mateusz Bąkała <maba@netcompany.com>
2022-12-14 09:37:42 +0100

    Day 14

--- Days/Day14.cs
+++ Days/Day14.cs
@@ -2,20 +2,76 @@
 
 namespace AdventOfCode2022.Days
 {
-    public class Day14 : AocDay<string>
+    public class Day14 : AocDay<(int, int)[]>
     {
-        public Day14(IInputParser<string> inputParser) : base(inputParser)
+        public Day14(IInputParser<(int, int)[]> inputParser) : base(inputParser)
         {
         }
 
-        protected override void Part1(string input)
+        protected override void Part1((int, int)[] input)
         {
-            Console.WriteLine(input);
+            var arr = BuildArray(input);
+            (int, int)? s;
+            int i = 0;
+            while((s = MoveSand(arr)) != null)
+            {
+                var (x, y) = s.Value;
+                arr[y, x] = true;
+                i++;
+            }
+            Console.WriteLine(i);
         }
 
-        protected override void Part2(string input)
+        protected override void Part2((int, int)[] input)
         {
-            Console.WriteLine(input);
+            var arr = BuildArrayWithFloor(input);
+            (int, int)? s;
+            int i = 0;
+            while ((s = MoveSand(arr)) != null)
+            {
+                var (x, y) = s.Value;
+                if (arr[y, x])
+                    break;
+                arr[y, x] = true;
+                i++;
+            }
+            Console.WriteLine(i);
+        }
+
+        private static bool[,] BuildArray((int, int)[] input)
+        {
+            var arr = new bool[input.Max(x => x.Item2)+1, 1000];
+            foreach(var (x,y) in input)
+                arr[y,x] = true;
+            return arr;
+        }
+
+        private static bool[,] BuildArrayWithFloor((int, int)[] input)
+        {
+            var arr = new bool[input.Max(x => x.Item2) + 3, 1000];
+            foreach (var (x, y) in input)
+                arr[y, x] = true;
+            for (int i = 0; i < 1000; i++)
+                arr[arr.GetLength(0) - 1, i] = true;
+            return arr;
+        }
+
+        private static (int,int)? MoveSand(bool[,] arr)
+        {
+            var s = (500, 0);
+            while (s.Item2 < arr.GetLength(0)-1)
+            {
+                var (x, y) = s;
+                if (!arr[y + 1, x])
+                    s = (x, y + 1);
+                else if (!arr[y + 1, x - 1])
+                    s = (x - 1, y + 1);
+                else if (!arr[y + 1, x + 1])
+                    s = (x + 1, y + 1);
+                else 
+                    return s;
+            }
+            return null;
         }
     }
 }
---
+++ Input/LineSegmentsParser.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode2022.Input
+{
+    public class LineSegmentsParser : IInputParser<(int, int)[]>
+    {
+        public (int, int)[] ParseInput(string input)
+        {
+            var lines = input.Split(Environment.NewLine);
+            List<(int, int)> coords = new();
+            foreach(var l in lines)
+            {
+                var seg = l.Split(" -> ").Select(a => (int.Parse(a.Split(",")[0]), int.Parse(a.Split(",")[1]))).ToArray();
+                for(int i = 1; i < seg.Length; i++)
+                {
+                    if (seg[i-1].Item1 == seg[i].Item1)
+                    {
+                        int x = seg[i - 1].Item1;
+                        for (int y = Math.Min(seg[i - 1].Item2, seg[i].Item2); y <= Math.Max(seg[i - 1].Item2, seg[i].Item2); y++)
+                            coords.Add((x, y));
+                    }
+                    else
+                    {
+                        int y = seg[i - 1].Item2;
+                        for (int x = Math.Min(seg[i - 1].Item1, seg[i].Item1); x <= Math.Max(seg[i - 1].Item1, seg[i].Item1); x++)
+                            coords.Add((x, y));
+                    }
+                }
+            }
+            return coords.Distinct().ToArray();
+        }
+    }
+}
